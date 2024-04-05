7d15c5159292b76acc4e3dba696ec83d7d60b965
Joe Hillman <joerhillman@gmail.com>
2024-04-05 18:20:45 -0400

    textSemantics

---
+++ stories/html/inlineTextSemantics.stories.js
@@ -0,0 +1,41 @@
+export default {
+  title: 'HTML/InlineTextSemantics'
+};
+
+export const anchor = () => '<a>Blockquote</a>';
+export const abbreviation = () => '<abbr>Abbreviation</abbr>';
+
+export const b = () => '<b>B</b>';
+export const bdi = () => '<bdi>BDI</bdi>';
+export const bdo = () => '<bdo>BDO</bdo>';
+export const br = () => 'BR <br/>';
+
+export const cite = () => '<cite>Cite</cite>';
+export const code = () => '<code>Code</code>';
+export const data = () => '<data>Data</data>';
+
+export const dfn = () => '<dfn>DFN</dfn>';
+export const em = () => '<em>EM</em>';
+export const i = () => '<i>I</i>';
+
+export const kbd = () => '<kbd>KBD</kbd>';
+export const mark = () => '<mark>Mark</mark>';
+export const q = () => '<q>Q</q>';
+
+export const rp = () => '<rp>RP</rp>';
+export const rt = () => '<rt>RT</rt>';
+export const ruby = () => '<ruby>Ruby</ruby>';
+
+export const s = () => '<s>S</s>';
+export const samp = () => '<samp>Samp</samp>';
+export const small = () => '<small>Small</small>';
+
+export const span = () => '<span>Span</span>';
+export const strong = () => '<strong>Strong</strong>';
+export const sub = () => '<sub>Sub</sub>';
+
+export const time = () => '<time>Time</time>';
+export const u = () => '<u>U</u>';
+export const htmlVar = () => '<var>Var</var>';
+
+export const wbr = () => '<wbr>WBR</wbr>';
